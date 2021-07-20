e365a8046c667d15a2309e7ce25fb679124d03b6
Soliloquy <ibrahia@ucalgary.ca>
2021-07-20 04:44:11 +0000

    complete exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,8 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Jesus", last_name: "Christ", hourly_rate: 30)
+@store2.employees.create(first_name: "Eve", last_name: "Adam", hourly_rate: 45)
+@store2.employees.create(first_name: "Kamaru", last_name: "Usman", hourly_rate: 100)
+
